ca89e04c56f4b2b3d56d6b6dbd188eadf6bf5a70
Halie Murray-Davis <hmurraydavis@yahoo.com>
2015-03-12 03:06:42 -0400

    parses out US data into csv file!

--- csv/csvFAOSTAT.py
+++ csv/csvFAOSTAT.py
@@ -5,29 +5,58 @@ import pprint
 import matplotlib.pyplot as plt
 import sympy.plotting as tplt
 
-def makeFAOSTATdf(data='short'):
-    if data=='short':
+#United States of America
+FullData = open('Production_Crops_E_Americas_1.csv', 'r')
+with open("USData.csv", "a") as usadata:
+    for line in FullData: 
+        if "United States of America" in line: 
+            usadata.write(line)
+            
+            
+#fullDF = pandas.DataFrame.from_csv(FullData)
+#with open("USData.csv", "a") as usadata:
+#    for index, row in fullDF.iterrows():
+#        if row['Country'] == 'United States of America':
+#            usadata.write(row)
+        
+
+def makeFAOSTATdf(region='short'):
+    '''Make a pandas dataframe from the data in a given CSV file'''
+    if region=='short':
         data=open('FeedGrains.csv', 'r')
-    elif data=='long':
+    elif region=='middle':
         data=open('FAOSTAT_Full.csv', 'r')
+    elif region=='world':
+        data=open('full_FAOSTAT.csv', 'r')
+    elif region=='americas':
+        data=open('Production_Crops_E_Americas_1.csv', 'r')
     else: 
         raise Exception("Unknown data form. Please specify your data file with the data key in the function call")
     return pandas.DataFrame.from_csv(data)
 
-df = makeFAOSTATdf()
-cropYear={}
-for index, row in df.iterrows():
-    if row['SC_GroupCommod_Desc'] == 'Corn':
-        if row['Year_ID'] in cropYear.keys():
-            cropYear[row['Year_ID']]=cropYear[row['Year_ID']]+ row['Amount']
-        else:
-            cropYear[row['Year_ID']] = row['Amount']
-            
+
+def sumYieldYears(crop='Corn'):
+    cropYear={}
+    cropDenotation='Item'
+    yearDenotation = 'Year'
+    amountDenotation = 'Value'
+    for index, row in df.iterrows():
+        if row[cropDenotation] == crop:
+            if row[yearDenotation] in cropYear.keys():
+                cropYear[row[yearDenotation]]=cropYear[row[yearDenotation]]+ row[amountDenotation]
+            else:
+                cropYear[row[yearDenotation]] = row[amountDenotation]
+    return cropYear
+
+##cropsWeCareAbout = ['Corn', 'Cotton lint', 'Soybeans', 'Wheat']
+##df = makeFAOSTATdf(region='world')
+##cropYear = sumYieldYears()
+
 #pprint.pprint(zip(cropYear.keys(),cropYear.values()))
-plt.plot(cropYear.keys(),cropYear.values(), linewidth=4.0)
-plt.ylabel('Year', fontsize=17)
-plt.xlabel('Busshels Produced', fontsize=17)
-plt.title('Corn Production Over Time', fontsize=22)
-plt.show()
+##plt.plot(cropYear.keys(),cropYear.values(), linewidth=4.0)
+##plt.ylabel('Year', fontsize=17)
+##plt.xlabel('Busshels Produced', fontsize=17)
+##plt.title('Corn Production Over Time', fontsize=22)
+##plt.show()
 #np.histogram(cropYear.keys(),cropYear.values())
 #pprint.pprint(cropYear)
