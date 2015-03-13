b70f79cf40a7ac997d3f2eb302efc730da688115
Halie Murray-Davis <hmurraydavis@yahoo.com>
2015-03-13 07:56:47 -0400

    histogram error still present. most likely data error

--- csv/csvFAOSTAT.py
+++ csv/csvFAOSTAT.py
@@ -4,6 +4,7 @@ import scipy
 import pprint
 import matplotlib.pyplot as plt
 import sympy.plotting as tplt
+import pylab as P
 
 def parseOutCountryData(country="United States of America", fileout = "USData.csv", filein = 'Production_Crops_E_Americas_1.csv'):
     FullData = open(filein, 'r')
@@ -55,15 +56,51 @@ def cropScatterplotByYear(cropYear, crop):
     plt.title(crop+' Production Over Time', fontsize=22)
     plt.show()
 
+
+def makeHistogramByCrop(yearList, cropYear, crop):
+    bins=[]
+    crops=[]
+    years=[]
+    for year in yearList:
+        years.append(yearList[year])
+        crops.append(cropYear[yearList[year]])
+###    for year in yearList.values():
+###        if (year%5) == 0:
+###            bins.append(year)
+    pprint.pprint(zip(years,crops))
+    years=np.array(years)
+    crops=np.array(crops)
+    n, bins, patches = P.hist(years, crops, normed=1, histtype='bar', rwidth=0.8)
+    P.show()
+    
+    
 #parseOutCountryData()
 df = makeFAOSTATdf(region='us')
 cropsWeCareAbout = ['Corn', 'Cotton lint', 'Soybeans', 'Wheat']
 yearList = buildYearList()
-for crop in cropsWeCareAbout:
-    cropYear = sumYieldYears(yearList, crop=crop)
-    cropScatterplotByYear(cropYear, crop)
+#for crop in cropsWeCareAbout:
+#    cropYear = sumYieldYears(yearList, crop=crop)
+#    cropScatterplotByYear(cropYear, crop)
 
 ##cropYear = sumYieldYears(yearList, crop='Spinach')
 ##cropScatterplotByYear(cropYear, 'Spinach')
+
+
+##mu, sigma = 200, 25
+##x = mu + sigma*P.randn(10000)
+##bins = [100,125,150,160,170,180,190,200,210,220,230,240,250,275,300]
+### the histogram of the data with histtype='step'
+##n, bins, patches = P.hist(x, bins, normed=1, histtype='bar', rwidth=0.8)
+
+###
+### now we create a cumulative histogram of the data
+###
+##P.figure()
+##P.show()
+
+
+            
+cropYear = sumYieldYears(yearList, crop='Spinach')
+makeHistogramByCrop(yearList, cropYear, 'Spinach')
 
 
