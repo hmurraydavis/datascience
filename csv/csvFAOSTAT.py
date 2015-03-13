21d577c3b99047b7cda781bea8bf049ae0b6fb0f
Halie Murray-Davis <hmurraydavis@yahoo.com>
2015-03-13 08:11:58 -0400

    linear fit code in, but erroring

--- csv/csvFAOSTAT.py
+++ csv/csvFAOSTAT.py
@@ -73,34 +73,49 @@ def makeHistogramByCrop(yearList, cropYear, crop):
     n, bins, patches = P.hist(years, crops, normed=1, histtype='bar', rwidth=0.8)
     P.show()
     
+def cropScatterplotByYearLinFit(cropYear, crop):
+    x=cropYear.keys(); y=cropYear.values()
+    plt.plot(x,y, 'r*', linewidth=4.0)
+    m,b = np.polyfit(x, y, 1) 
+    plt.plot(x, m*x+b, 'k',linewidth=3.0) 
+    plt.xlabel('Year', fontsize=17)
+    plt.ylabel('Tonnes Produced', fontsize=17)
+    plt.title(crop+' Production Over Time', fontsize=22)
+    plt.show()
+    
+    
+def runScatterForCropsWeCareAbout():
+    cropsWeCareAbout = ['Corn', 'Cotton lint', 'Soybeans', 'Wheat']
+    yearList = buildYearList()
+    resultsDict={}
+    for crop in cropsWeCareAbout:
+        cropYear = sumYieldYears(yearList, crop=crop)
+        cropScatterplotByYear(cropYear, crop)
+        resultsDict[crop] =cropYear
+    return resultsDict
+
+
+def histTest():
+    mu, sigma = 200, 25
+    x = mu + sigma*P.randn(10000)
+    bins = [100,125,150,160,170,180,190,200,210,220,230,240,250,275,300]
+    # the histogram of the data with histtype='step'
+    n, bins, patches = P.hist(x, bins, normed=1, histtype='bar', rwidth=0.8)
+    # now we create a cumulative histogram of the data
+    P.figure()
+    P.show()
+    
     
 #parseOutCountryData()
 df = makeFAOSTATdf(region='us')
-cropsWeCareAbout = ['Corn', 'Cotton lint', 'Soybeans', 'Wheat']
-yearList = buildYearList()
-#for crop in cropsWeCareAbout:
-#    cropYear = sumYieldYears(yearList, crop=crop)
-#    cropScatterplotByYear(cropYear, crop)
+#runScatterForCropsWeCareAbout()
 
 ##cropYear = sumYieldYears(yearList, crop='Spinach')
 ##cropScatterplotByYear(cropYear, 'Spinach')
 
-
-##mu, sigma = 200, 25
-##x = mu + sigma*P.randn(10000)
-##bins = [100,125,150,160,170,180,190,200,210,220,230,240,250,275,300]
-### the histogram of the data with histtype='step'
-##n, bins, patches = P.hist(x, bins, normed=1, histtype='bar', rwidth=0.8)
-
-###
-### now we create a cumulative histogram of the data
-###
-##P.figure()
-##P.show()
-
-
-            
+yearList = buildYearList()        
 cropYear = sumYieldYears(yearList, crop='Spinach')
-makeHistogramByCrop(yearList, cropYear, 'Spinach')
+#makeHistogramByCrop(yearList, cropYear, 'Spinach')
+#cropScatterplotByYearLinFit(cropYear,  'Spinach')
 
 
