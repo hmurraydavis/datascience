bfe61d367ff4577841228283fd113ccc575ddc72
Halie Murray-Davis <hmurraydavis@yahoo.com>
2015-03-22 23:10:47 -0400

    remove histogram test code gathered from internet

--- csv/csvFAOSTAT.py
+++ csv/csvFAOSTAT.py
@@ -172,18 +172,6 @@ def runScatterForCropsWeCareAbout():
         cropScatterplotByYear(cropYear, crop)
         resultsDict[crop] =cropYear
     return resultsDict
-
-
-def histTest():
-    '''test of histogram functionality from the interwebs!'''
-    mu, sigma = 200, 25
-    x = mu + sigma*P.randn(10000)
-    bins = [100,125,150,160,170,180,190,200,210,220,230,240,250,275,300]
-    # the histogram of the data with histtype='step'
-    n, bins, patches = P.hist(x, bins, normed=1, histtype='bar', rwidth=0.8)
-    # now we create a cumulative histogram of the data
-    P.figure()
-    P.show()
     
     
 def makePMF(cropYear):
@@ -254,5 +242,3 @@ def plotManyCropDerivsOverTime(yearList, crops):
 #TODO: 
 ##PMF
 ##PDF
-##CDF
-
